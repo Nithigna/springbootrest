2faaec916f7a8d72cc72a6d429b0756d195b8afa
bujji <bujji@DESKTOP-IT81UEL>
2021-05-12 15:10:50 +0530

    spring boot rest

---
+++ src/SecurityConfiguration.java
@@ -0,0 +1,43 @@
+package com.example.demorest;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		//set the config on the auth object
+		auth.inMemoryAuthentication()
+		.withUser("navneetu")
+		.password("nitigna")
+		.roles("USER")
+		.and()
+		.withUser("chandra")
+		.password("akshay")
+		.roles("ADMIN");
+	}
+	
+	@Bean
+	public PasswordEncoder getPasswordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+		.antMatchers("/admin").hasRole("ADMIN")
+		.antMatchers("/user").hasAnyRole("USER","ADMIN")//please allow access to /user url to both user and admin
+		.antMatchers("/").permitAll()
+		//.hasAnyRole("_","_")
+		.and()
+		.formLogin();
+	}
+	
+}
